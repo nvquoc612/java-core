600278b9f7a3a3cd49877ff7aa56290fdb4fe8b8
quoc <nvquocc@hotmail.com>
2021-01-12 01:11:24 +0700

    howkteam java core update 12-01-2021

--- src/howkteam/coban/toantu/ToanTu.java
+++ src/howkteam/coban/toantu/ToanTu.java
@@ -1,5 +1,15 @@
 package howkteam.coban.toantu;
 
 public class ToanTu {
-
+    public static void main(String[] args) {
+        int a = 1;
+        //In rồi mới tăng
+        System.out.println(a++);
+        // Tăng rồi mới in
+        System.out.println(++a);
+//         Cộng với 1 và in ra rồi mới tăng
+        System.out.println(a+++1);
+        //Tăng rồi mới cộng 1 và in ra
+        System.out.println(++a+1);
+    }
 }
